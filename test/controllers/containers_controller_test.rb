7699d518e50751acd54c17a2d0dfca385e9b2fbd
root <root@localhost.localdomain>
2022-06-05 18:59:14 +0100

    Revert

--- test/controllers/containers_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ControllersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get controllers_index_url
-    assert_response :success
-  end
-end
